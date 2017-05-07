fcab780f8f7c0fe5c4660eb830d5e65d40301ad2
A A <abcdefghijklm@outlook.jp>
2017-05-07 15:20:10 +0900

    Initial commit

---
+++ src/main/java/jp/ac/hiroshima_u/fu_midori/SSH2017/FxMain.java
@@ -0,0 +1,16 @@
+package jp.ac.hiroshima_u.fu_midori.SSH2017;
+
+/**
+ * GUI時のMainクラス。
+ *
+ * @author 遠藤拓斗 on 2017/05/07.
+ */
+public class FxMain {
+    /**
+     * メインクラス。
+     * @param args args
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
